30890063197ec09dce2e9d3e4daa619c17be9269
wozniak04 <mikimen321@gmail.com>
2021-10-11 15:32:38 +0200

    walidacja wielkości liter przy haśle

--- walidacja.js
+++ walidacja.js
@@ -1,6 +1,16 @@
 //zaczne sprawdzania loginów i haseł jak formularz rejestracji 
 //będzie gotowy, ponieważ nie chce zrobić niepotrzebnych błędów
-function sprawdzanie_rejestracji()
+function sprawdz_haslo()
 {
-    alert("działa");
+    let haslo=document.getElementById("logowanie_h").value;
+    let duza=0;   
+   
+    for(var l=0;l<haslo.length;l++)
+    {
+        if(haslo[l].charCodeAt()>64&&haslo[l].charCodeAt()<91)
+            duza++;
+    }
+        alert(duza);
+    
+    
 }
