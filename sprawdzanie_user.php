0427310babc74fa9bcc3be65c7506319d4bf5794
ARECKI23 <arkadiuszguth@gmail.com>
2021-10-13 15:00:34 +0200

    to działa

--- sprawdzanie_user.php
+++ sprawdzanie_user.php
@@ -3,13 +3,14 @@
     require_once "connect.php";
     $polaczenie= mysqli_connect($host, $db_user, $db_password, $db_name);
 
-        if(mysqli_connect_errno())
+        /*if(mysqli_connect_errno())
             echo '<h1>git</h1>';
         else
             echo "<h1>.blad.</h1>";
+*/
 
-
-    $sql="SELECT username FROM users WHERE id=1 ";
+    
+    $sql="SELECT username FROM users WHERE username = 'Arek';";
     $wynik = mysqli_query($polaczenie, $sql);
     while ($row = mysqli_fetch_array($wynik))
 {
