c4087f4f54dc15889971920ff36253161e353b18
ARECKI23 <arkadiuszguth@gmail.com>
2021-10-14 23:15:11 +0200

    .

--- index_rejestracja.php
+++ index_rejestracja.php
@@ -15,7 +15,7 @@
             <form action="register.php" method="post">
                 
                 
-                <input type="name" value="" name="Username" placeholder="Username" id="logowanie_u" required onfocusout="sprawdzanie_czy_uzytkownik_istnieje()"><br><br>
+                <input type="text" value="" name="Username" placeholder="Username" id="logowanie_u" required onfocusout="sprawdzanie_czy_uzytkownik_istnieje()"><br><br>
                 <p id="blad_username">wprowadź nazwe użytkownika</p>
                 
                 <input type="password" value="" name="password" placeholder="Haslo" id="logowanie_h" required onkeypress="sprawdz_haslo()" onclick="czysc1()"><br><br>
@@ -25,12 +25,12 @@
                 <input type="password" value="" name="password1" placeholder="Powtorz haslo" id="logowanie_h1" required onfocusout="porownanie()" onclick="czysc2()"><br><br>
                 <p id="blad_password_powtorz">hasło powinno być takie same</p>
                 
-                <input type="email" value="" name="email" placeholder="Email" id="logowanie_e" required onfocusout="sprawdzanie_email()"><br><br>
+                <input type="text" value="" name="email" placeholder="Email" id="logowanie_e" required onfocusout="sprawdzanie_email()"><br><br>
                 <p id="blad_email">email powinien posiadać 6 liter przed @</p>
                 
-                <input type="name" value="" name="name" placeholder="Imie" id="logowanie_n" required onfocusout="imie()"><br><br>
+                <input type="text" value="" name="name" placeholder="Imie" id="logowanie_n" required onfocusout="imie()"><br><br>
                 <p id="blad_imie">wpisz imie</p>
-                <input type="name" value="" name="surname" placeholder="Nazwisko" id="logowanie_s" required onfocusout="nazwisko()"><br><br>
+                <input type="text" value="" name="surname" placeholder="Nazwisko" id="logowanie_s" required onfocusout="nazwisko()"><br><br>
                 <p id="blad_nazwisko">wpisz nazwisko</p>
                 <input type="date" value="" name="date-acc" id="logowanie_d" min="1900-01-01" max="2021-12-31"required onfocusout=""><br><br>
                 
