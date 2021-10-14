3ebdd0fbf1d9e2788b472cf32df5a7c284f882be
wozniak04 <mikimen321@gmail.com>
2021-10-14 11:47:38 +0200

    Update sprawdzanie_user.php

--- sprawdzanie_user.php
+++ sprawdzanie_user.php
@@ -1,21 +1,21 @@
 <?php
-    $jso="";
+    $jso=null;
     $q = $_REQUEST['q'];
-    require_once "connect.php";
-    $polaczenie= mysqli_connect($host, $db_user, $db_password, $db_name);
+    // require_once "connect.php";
+    // $polaczenie= mysqli_connect($host, $db_user, $db_password, $db_name);
 
         
 
    
-        $sql="SELECT username FROM users WHERE username='$q';";
-        $wynik = mysqli_query($polaczenie, $sql);
+    //     $sql="SELECT username FROM users WHERE username='$q';";
+    //     $wynik = mysqli_query($polaczenie, $sql);
 
 
-        while ($row = mysqli_fetch_array($wynik))
-        {
-           $jso=$row['username'];
-        }
-            // $jso=$q;
+    //     while ($row = mysqli_fetch_array($wynik))
+    //     {
+    //        $jso=$row['username'];
+    //     }
+             $jso=$q;
     echo json_encode($jso);
 
 
