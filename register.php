dee5ae4f845c755dcc49df14068cfb140fa01757
ARECKI23 <arkadiuszguth@gmail.com>
2021-10-14 12:55:00 +0200

    rejestracja php
    skrypt wstawiający dane do bazy danych

--- register.php
+++ register.php
@@ -3,21 +3,24 @@
 require_once "connect.php";
 require_once "";
 
-// $username_ = $_POST['username'];
-// $password_ = $_POST['password'];
-// $name_ = $_POST['name'];
-// $surname_ = $_POST['surnname'];
-// $email_ = $_POST['email'];
-// $date_born_ = $_POST['date_born'];
+$username_ = $_POST['username'];
+$password_ = $_POST['password'];
+$name_ = $_POST['name'];
+$surname_ = $_POST['surnname'];
+$email_ = $_POST['email'];
+$date_born_ = $_POST['date_born'];
 
 
 
-$zapytanie = "INSERT INTO users (name, surnname, username, date_born, email) VALUES ('$name_','$surname_','$username_','$date_born_','$email_','')" or die(mysql_error());
+$zapytanie = INSERT INTO users
+(`id`, `username`, `password`, `created_at`, `admin`, `name`, `surname`, `date_born`, `email`, `gender`) 
+VALUES 
+('','$username_','$password_','','$admin','$name','$surname','$date_born_','$email','$gender');
 	
 	$wynik = mysql_query($zapytanie);
 	
 	if ($wynik) {
-		echo 'Prawidłowo dodano do bazy danych';	
+		header('Location: index_glowna.php');	
 	}
  
 	mysql_close($db);		
