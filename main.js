aabfac8f5683ba00a4b9f8fada076e4cac4d5d56
wozniak04 <mikimen321@gmail.com>
2021-10-23 23:48:23 +0200

    nic

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let x=0;
 function openForm() {
     document.getElementById("myForm").style.display = "block";
   }
@@ -5,3 +6,16 @@ function openForm() {
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
+function specjalisci()
+{ 
+    
+    if(x<1){
+      x+=0.01;
+      setTimeout(specjalisci,1);
+      
+    }
+    else if(x>=1){
+      alert();
+       x=0;
+    }
+}
