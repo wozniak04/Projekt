cfaf753d6f402202b0771140ec3afb78643b498b
wozniak04 <mikimen321@gmail.com>
2021-10-24 12:33:05 +0200

    naprawienie walidacji email

--- walidacja.js
+++ walidacja.js
@@ -1,5 +1,5 @@
-
-
+let pick;
+let dataa;
 const wysli=document.querySelector("#przycisk_dalej_logowanie");
 wysli.disabled=true;
 var g=0;
@@ -50,6 +50,75 @@ function czysc2()
     document.querySelector("#logowanie_h1").value="";
 }
 
+function sprawdzanie_czy_uzytkownik_istnieje()
+{
+    
+    let odp;
+    const xmlhttp = new XMLHttpRequest();
+    const user = document.querySelector("#logowanie_u").value;
+    const blad=document.querySelector("#blad_username");
+    
+    xmlhttp.onload = function () 
+    {
+            odp=JSON.parse(this.response);
+            if(odp==user)
+                blad.innerHTML="podana nazwa uzytkownika istnieje";
+             else
+             {
+                blad.innerHTML = "";
+                odb()
+             }
+    }
+    
+    xmlhttp.open("GET","sprawdzanie_user.php?q=" + user,true);
+    xmlhttp.send();
+    
+}
+
+function sprawdzanie_email()
+{   
+    var mal=0;
+    let poprawnye=true;
+    const email=document.querySelector("#logowanie_e").value;
+    const blad_e=document.querySelector("#blad_email");
+    
+    for(var i=0;i<email.length;i++)
+    {
+        if(email[i]=='@')
+            mal=i;
+        if(mal<=6)
+        {
+            blad_e.innerHTML="przed @ powinno być 6 znaków";
+            poprawnye=false;
+        }
+        else
+        {
+            blad_e.innerHTML="";
+            poprawnye=true;
+        }
+    }
+
+    if(poprawnye==true)
+    {   
+        let odp;
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.onload = function () 
+        {
+            odp=JSON.parse(this.response);
+            if(odp==email)
+                blad_e.innerHTML="podany email istnieje";
+            else
+            {
+                blad_e.innerHTML = "";
+                odb()
+            }
+       }
+        xmlhttp.open("GET","sprawdzanie_email.php?q=" + email,true);
+        xmlhttp.send();
+    }
+}
+
+
 function imie()
 {
     const blad=document.querySelector("#blad_imie");
@@ -83,13 +152,16 @@ function imie()
 function wybor(wyb)
 {
     odb()
+    pick=wyb;
 }
 
 function data(date)
 {
     if(date!="")
+    {
+        dataa=date;
         odb();
-
+    }
 }
 
 function odb()
@@ -101,10 +173,9 @@ function odb()
     const powtorz=document.querySelector("#blad_password_powtorz").value;
     const uzytkownik=document.querySelector("#blad_username").value;
     const email=document.querySelector("#blad_email").value;
-    const wyb1=document.querySelector("#kobieta_box").value;
-    const wyb2=document.querySelector("#mezczyzna_box").value;
-    const dat=document.querySelector("#logowanie_d").value;
+    
+    
 
-    if(g>=8 && haslo==undefined && powtorz==undefined && uzytkownik==undefined && email==undefined && imie==undefined && nazwisko==undefined &&(wyb1=="Kobieta"||wyb2=="Mezczyzna")&&dat!="" )
+    if(g>=8 && haslo==undefined && powtorz==undefined && uzytkownik==undefined && email==undefined && imie==undefined && nazwisko==undefined &&pick!=""&&dataa!="" )
         wysli.disabled=false;
 }
