21e2af99c50486a1420d450dd52048cd15346550
wozniak04 <mikimen321@gmail.com>
2021-10-26 12:46:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -16,18 +16,16 @@ function specjalisci()
       as.play();
     }, 1000);
   }
-}
 
-function setings()
-{ 
-  if(plecc=='m')
+  if(plecc=='k')
   {
     czas=setTimeout(() => {
-      as=new Audio("asystent/meski/ustawienia.mp3");
+      as=new Audio("asystent/zenski/nasi.mp3");
       as.play();
     }, 1000);
   }
 }
+
 function stop()
 {
     as.load();
@@ -43,6 +41,14 @@ function towary()
       as.play();
     }, 1000);
   }
+
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/towary.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function nasz()
@@ -51,6 +57,13 @@ function nasz()
   {
     czas=setTimeout(() => {
       as=new Audio("asystent/meski/sklepy.mp3");
+      as.play();
+    }, 1000);
+  }
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/sklepy.mp3");
       as.play();
     }, 1000);
   }
@@ -65,108 +78,257 @@ function onas()
     as.play();
   }, 1000);
   }
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/nas.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function sinu()
-{
+{ 
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/sinupret.mp3");
     as.play();
   }, 1000);
-
+  }
+  
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/sinupret.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 function calcu()
-{
+{ 
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/calcium.mp3");
     as.play();
   }, 1000);
+  } 
+
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/calcium.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function grip()
-{
+{ 
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/gripex.mp3");
     as.play();
   }, 1000);
+  } 
+  
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/gripex.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function envil()
 {
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/envil.mp3");
     as.play();
   }, 1000);
+  }
+
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/envil.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function voltar()
-{
+{ 
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/voltaren.mp3");
     as.play();
   }, 1000);
+  }
+
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/voltaren.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function fer()
-{
+{ 
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/fervex.mp3");
     as.play();
   }, 1000);
+  }
+
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/fervex.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function enterol()
 {
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/enetrol.mp3");
     as.play();
   }, 1000);
+  }
+
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/enterol.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function otri()
 {
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/otrivin.mp3");
     as.play();
   }, 1000);
+  }
+
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/otrivin.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function molek()
 {
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/molekind3.mp3");
     as.play();
   }, 1000);
+  }
+
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/molekind3.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function arge()
 {
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/argentint.mp3");
     as.play();
   }, 1000);
+  }
+
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/argentint.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function yuwe()
 {
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/termometr.mp3");
     as.play();
   }, 1000);
+  }
+
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/terrmometr.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function octa()
 {
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/octanisept.mp3");
     as.play();
   }, 1000);
+  }
+
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/octanisept.mp3");
+      as.play();
+    }, 1000);
+  }
 }
 
 function ustawienia()
-{
+{ 
+  if(plecc=='m')
+  {
   czas=setTimeout(() => {
     as=new Audio("asystent/meski/ustawienia.mp3");
     as.play();
   }, 1000);
+  }
+
+  if(plecc=='k')
+  {
+    czas=setTimeout(() => {
+      as=new Audio("asystent/zenski/ustawienia.mp3");
+      as.play();
+    }, 1000);
+  }
 }
